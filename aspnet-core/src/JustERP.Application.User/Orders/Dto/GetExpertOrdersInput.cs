e6f7d21a931abafd59bd7e7f7abb9521cd2d0895
liqiang <mzgs@foxmail.com>
2018-01-10 21:00:34 +0800

    默认分页数

--- aspnet-core/src/JustERP.Application.User/Orders/Dto/GetExpertOrdersInput.cs
+++ aspnet-core/src/JustERP.Application.User/Orders/Dto/GetExpertOrdersInput.cs
@@ -2,9 +2,14 @@
 
 namespace JustERP.Application.User.Orders.Dto
 {
-    public class GetExpertOrdersInput : PagedResultRequestDto
+    public class GetExpertOrdersInput : DefaultPagedResultRequestDto
     {
         public long ExpertId { get; set; }
         public long ServerExpertId { get; set; }
+    }
+
+    public class DefaultPagedResultRequestDto : PagedResultRequestDto
+    {
+        protected DefaultPagedResultRequestDto() => MaxResultCount = 100;
     }
 }
