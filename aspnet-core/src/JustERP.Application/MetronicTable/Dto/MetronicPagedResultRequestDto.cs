337b9d038ca1f4f2d283975b8dad1af798502fee
liqiang <mzgs@foxmail.com>
2018-02-04 23:26:51 +0800

    fix pagination not working

--- aspnet-core/src/JustERP.Application/MetronicTable/Dto/MetronicPagedResultRequestDto.cs
+++ aspnet-core/src/JustERP.Application/MetronicTable/Dto/MetronicPagedResultRequestDto.cs
@@ -1,14 +1,21 @@
 ﻿using Abp.Application.Services.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JustERP.MetronicTable.Dto
 {
     public class MetronicPagedResultRequestDto : PagedResultRequestDto
     {
+        [FromQuery(Name = "pagination[page]")]
         public int Page { get; set; } = 1;
+        [FromQuery(Name = "pagination[pages]")]
         public int Pages { get; set; }
+        [FromQuery(Name = "pagination[perpage]")]
         public int Perpage { get; set; } = 10;
+        [FromQuery(Name = "pagination[total]")]
         public int Total { get; set; }
+        [FromQuery(Name = "sort[sort]")]
         public string Sort { get; set; }
+        [FromQuery(Name = "sort[field]")]
         public string Field { get; set; }
 
         public override int SkipCount => (Page - 1) * Perpage;
---
+++ aspnet-core/src/JustERP.Application/MetronicTable/ModelBinder/MetronicModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace JustERP.MetronicTable.ModelBinder
+{
+    public class MetronicModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            //ComplexTypeModelBinder
+            var modelCreator = ((Expression<Func<object>>)(() => Expression.New(bindingContext.ModelType))).Compile();
+            bindingContext.Model = modelCreator();
+            foreach (var property in bindingContext.ModelMetadata.Properties)
+            {
+                var modelName = property.BinderModelName ?? $"query[{property.PropertyName.ToLower()}]";
+                var value = bindingContext.ValueProvider.GetValue(modelName);
+                property.PropertySetter(bindingContext.Model, value.FirstValue);
+            }
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
