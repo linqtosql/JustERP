b111e96fbbfb7fe23d5e7bda376549b9e59f2d21
liqiang <mzgs@foxmail.com>
2018-02-28 22:38:30 +0800

    add migration

--- aspnet-core/src/JustERP.EntityFrameworkCore/Migrations/20180228141547_UpdateAbpVersion.Designer.cs
+++ aspnet-core/src/JustERP.EntityFrameworkCore/Migrations/20180228141547_UpdateAbpVersion.Designer.cs
@@ -16,9 +16,10 @@ using System;
 namespace JustERP.Migrations
 {
     [DbContext(typeof(JustERPDbContext))]
-    partial class JustERPDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180228141547_UpdateAbpVersion")]
+    partial class UpdateAbpVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
