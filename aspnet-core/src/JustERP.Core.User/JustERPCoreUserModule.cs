6c6828134e751d1f082ac4bd278cb436bfa35d90
liqiang <mzgs@foxmail.com>
2018-05-06 13:29:34 +0800

    add abpzerocoremodule for coreusermodule

--- aspnet-core/src/JustERP.Core.User/JustERPCoreUserModule.cs
+++ aspnet-core/src/JustERP.Core.User/JustERPCoreUserModule.cs
@@ -1,9 +1,11 @@
 ﻿using Abp.Modules;
 using Abp.Reflection.Extensions;
+using Abp.Zero;
 
 namespace JustERP.Core.User
 {
-    [DependsOn(typeof(JustERPCoreModule))]
+    [DependsOn(typeof(JustERPCoreModule),
+        typeof(AbpZeroCoreModule))]
     public class JustERPCoreUserModule : AbpModule
     {
         public override void Initialize()
