e25e155f20278f67ba9b3fd060b60a19fcf3fb92
liqiang <mzgs@foxmail.com>
2018-05-06 13:24:27 +0800

    add missing file

---
+++ aspnet-core/src/JustERP.Core.User/JustERPCoreUserModule.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Modules;
+using Abp.Reflection.Extensions;
+
+namespace JustERP.Core.User
+{
+    [DependsOn(typeof(JustERPCoreModule))]
+    public class JustERPCoreUserModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(JustERPCoreUserModule).GetAssembly());
+        }
+    }
+}
