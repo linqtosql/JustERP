5e7f73fa6ca0e61d1d2f27504ae56b992c95dde5
liqiang <mzgs@foxmail.com>
2018-05-15 23:04:31 +0800

    。

---
+++ aspnet-core/src/JustERP.Core.User/Activities/EventHandler/InitLabels.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Dependency;
+using Abp.Events.Bus.Handlers;
+using JustERP.Core.User.Authorization.Event;
+
+namespace JustERP.Core.User.Activities.EventHandler
+{
+    public class InitLabels : IEventHandler<RegisterCompleteEventData>, ITransientDependency
+    {
+        public ActivityManager ActivityManager { get; set; }
+        public void HandleEvent(RegisterCompleteEventData eventData)
+        {
+            ActivityManager.InitLabels(eventData.Register);
+        }
+    }
+}
---
+++ aspnet-core/src/JustERP.Core.User/Authorization/Event/RegisterCompleteEventData.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Events.Bus;
+using JustERP.Core.User.Pepoles;
+
+namespace JustERP.Core.User.Authorization.Event
+{
+    public class RegisterCompleteEventData : EventData
+    {
+        public MtPeople Register { get; set; }
+    }
+}
